7112f784c36e2eb87e621b71fcb55ef3b4cf96b5
Odeh Daniels <odehdaniel1@gmail.com>
2021-01-08 22:24:23 +0100

    Header component initialization/NPM fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      {/* Header */}
+      <div className="app__header">
+        <Header />
+      </div>
+      {/* Body */}
+      <div className="app__body">
+
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from './robinhood.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header__wrapper">
+            {/* Logo */}
+            <div className="header__logo">
+                <img src={Logo} width={25} />
+            </div>
+            {/* Search Bar */}
+            <div className="header__search">
+                <div className="header__searchContainer">
+                    <input placeholder="Search" type="text" />
+               </div>
+            </div>
+            {/* Menu items */}
+            <div className="header__menuItems">
+                <a href="#">Free Stock</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Cash</a>
+                <a href="#">Messages</a>
+                <a href="#">Account</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
